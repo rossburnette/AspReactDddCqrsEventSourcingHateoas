b2586bbc536b30ae707da371e4b17283c3868562
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-15 21:21:37 +0300

    v1.0.2

---
+++ backend/common/YngStrs.Common/YngStrs.Common/ErrorType.cs
@@ -0,0 +1,11 @@
+﻿namespace YngStrs.Common
+{
+    public enum ErrorType
+    {
+        Validation = 400,
+        NotFound = 404,
+        Unauthorized = 401,
+        Conflict = 409,
+        Critical = 500
+    }
+}
