6652eeb6dcede82d499e990c898bd31539b66bdf
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-16 14:45:03 +0300

    v1.0.5

---
+++ backend/common/YngStrs.Common.EventSourcing/YngStrs.Common.EventSourcing/Business/EventBus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Marten;
+using MediatR;
+using YngStrs.Common.EventSourcing.Core;
+
+namespace YngStrs.Common.EventSourcing.Business
+{
+    /// <summary>
+    /// The root of the event sourcing.
+    /// </summary>
+    public class EventBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly IDocumentSession _session;
+
+        public EventBus(IMediator mediator, IDocumentSession session)
+        {
+            _mediator = mediator;
+            _session = session;
+        }
+
+        /// <summary>
+        /// Saves the events in the database and sends the notification message (<see cref="IEvent"/>).
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="streamId"></param>
+        /// <param name="events"></param>
+        /// <returns>Void.</returns>
+        public async Task<Unit> PublishAsync<TEvent>(Guid streamId, params TEvent[] events)
+            where TEvent : IEvent
+        {
+            foreach (var @event in events)
+            {
+
+                _session.Events.Append(streamId, @event);
+                await _mediator.Publish(@event);
+            }
+
+            await _session.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
