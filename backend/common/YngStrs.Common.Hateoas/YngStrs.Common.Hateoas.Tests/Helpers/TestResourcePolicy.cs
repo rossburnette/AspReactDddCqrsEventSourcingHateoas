03becca904dc68eb5a73d15b45399cab93337a0a
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-16 11:25:44 +0300

    v1.0.4

---
+++ backend/common/YngStrs.Common.Hateoas/YngStrs.Common.Hateoas.Tests/Helpers/TestResourcePolicy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RiskFirst.Hateoas;
+using YngStrs.Common.Hateoas.Core;
+
+namespace YngStrs.Common.Hateoas.Tests.Helpers
+{
+    public class TestResourcePolicy : IPolicy<TestResource>
+    {
+        public Action<LinksPolicyBuilder<TestResource>> PolicyConfiguration =>
+            builder => { builder.RequireSelfLink(); };
+    }
+}
