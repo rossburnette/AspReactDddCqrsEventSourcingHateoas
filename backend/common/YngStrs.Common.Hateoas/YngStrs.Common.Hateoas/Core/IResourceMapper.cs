44005ff81281ebeaf6664c0814229b0eeee08099
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-17 10:00:53 +0300

    v1.0.8

--- backend/common/YngStrs.Common.Hateoas/YngStrs.Common.Hateoas/Core/IResourceMapper.cs
+++ backend/common/YngStrs.Common.Hateoas/YngStrs.Common.Hateoas/Core/IResourceMapper.cs
@@ -1,7 +1,19 @@
-﻿namespace YngStrs.Common.Hateoas.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace YngStrs.Common.Hateoas.Core
 {
     public interface IResourceMapper
     {
-        
+        Task<TResource> MapAsync<T, TResource>(T source)
+            where TResource : Resource;
+
+        Task<TContainer> MapContainerAsync<T, TResource, TContainer>(IEnumerable<T> models)
+            where TContainer : ResourceContainer<TResource>, new()
+            where TResource : Resource;
+
+        Task<TResource> CreateEmptyResourceAsync<TResource>(Action<TResource> beforeMap = null)
+            where TResource : Resource, new();
     }
 }
