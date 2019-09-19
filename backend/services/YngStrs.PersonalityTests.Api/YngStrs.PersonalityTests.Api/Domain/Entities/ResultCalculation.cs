7796c72d9e2f781392c5d766963eb7479f00e1bf
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-19 16:39:59 +0300

    v.1.1.5

---
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/Domain/Entities/ResultCalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YngStrs.PersonalityTests.Api.Domain.Entities
+{
+    /// <summary>
+    /// Domain object represents the result of the user profiling.
+    /// </summary>
+    /// <remarks>
+    /// Linked with <see cref="UserTestResult"/>, <seealso cref=""/>.
+    /// </remarks>
+    public class ResultCalculation
+    {
+        public Guid TestResultId { get; set; }
+
+        public double Percentage { get; set; }
+    }
+}
