86d5910e1c16a75f2bdfacfc8405594f13673ac3
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-25 20:23:38 +0300

    v1.2.9

---
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/Hateoas/Resources/PersonalityTest/PersonalityTestMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using YngStrs.PersonalityTests.Api.Domain.Views.PersonalityTests;
+
+namespace YngStrs.PersonalityTests.Api.Hateoas.Resources.PersonalityTest
+{
+    public class PersonalityTestMappingProfile : Profile
+    {
+        public PersonalityTestMappingProfile()
+        {
+            CreateMap<InitPersonalityTestView, PersonalityTestResource>(MemberList.Destination);
+        }
+    }
+}
---
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/Hateoas/Resources/PersonalityTest/PersonalityTestResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using YngStrs.Common.Hateoas.Core;
+
+namespace YngStrs.PersonalityTests.Api.Hateoas.Resources.PersonalityTest
+{
+    public class PersonalityTestResource : Resource
+    {
+        public Guid QuestionId { get; set; }
+
+        public int QuestionNumber { get; set; }
+
+        public Guid OptionId { get; set; }
+
+        public string OptionDescription { get; set; }
+
+        public bool IsTextOnly { get; set; }
+
+        public string Base64Image { get; set; }
+    }
+}
