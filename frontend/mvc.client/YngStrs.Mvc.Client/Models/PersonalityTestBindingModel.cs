a18877cdb8a43d46b7263094684ddcaffc655b89
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-28 09:05:43 +0300

    v1.4.1

---
+++ frontend/mvc.client/YngStrs.Mvc.Client/Models/PersonalityTestBindingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace YngStrs.Mvc.Client.Models
+{
+    public class PersonalityTestBindingModel
+    {
+        public TestStats TestStats { get; set; }
+
+        public TestAnswers TestAnswers { get; set; }
+    }
+}
