df20516cb997aa3ceca2def52058023c04dc66b2
Jordan Jordanov <d_yugioh@abv.bg>
2019-10-10 10:52:24 +0300

    v1.7.3

--- frontend/mvc.client/YngStrs.Mvc.Client/Services/Core/IChatbotService.cs
+++ frontend/mvc.client/YngStrs.Mvc.Client/Services/Core/IChatbotService.cs
@@ -1,7 +1,11 @@
-﻿namespace YngStrs.Mvc.Client.Services.Core
+﻿using YngStrs.Mvc.Client.Models.Chatbot;
+
+namespace YngStrs.Mvc.Client.Services.Core
 {
     public interface IChatbotService
     {
         object GetQuestionsContent();
+
+        void ArrangeUserAnswers(ChatbotResultsRootObject rootObject);
     }
 }
