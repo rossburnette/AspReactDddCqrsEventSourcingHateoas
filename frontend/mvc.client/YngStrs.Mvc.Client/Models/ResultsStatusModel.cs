f9d6f7dca21f4bba6a84b3a69c2d2df05362f695
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-19 00:16:20 +0200

    v2.2.5

---
+++ frontend/mvc.client/YngStrs.Mvc.Client/Models/ResultsStatusModel.cs
@@ -0,0 +1,15 @@
+﻿namespace YngStrs.Mvc.Client.Models
+{
+    public class ResultsStatusModel
+    {
+        public ResultsStatusModel(bool status)
+        {
+            Status = status;
+            Errors = new string[0];
+        }
+
+        public bool Status { get; set; }
+
+        public string[] Errors { get; set; }
+    }
+}
