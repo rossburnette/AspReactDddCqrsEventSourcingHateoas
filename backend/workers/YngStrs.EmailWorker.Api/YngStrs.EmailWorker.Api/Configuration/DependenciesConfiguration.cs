7c50a0bb96cb53f2398a24dd3f193b20234846f7
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-18 13:56:03 +0200

    v2.2.2

---
+++ backend/workers/YngStrs.EmailWorker.Api/YngStrs.EmailWorker.Api/Configuration/DependenciesConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YngStrs.EmailWorker.Api.Services.Business;
+using YngStrs.EmailWorker.Api.Services.Core;
+
+namespace YngStrs.EmailWorker.Api.Configuration
+{
+    internal static class DependenciesConfiguration
+    {
+        internal static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ISendEmailService, SendEmailService>();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ backend/workers/YngStrs.EmailWorker.Api/YngStrs.EmailWorker.Api/Services/Business/SendEmailService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using YngStrs.EmailWorker.Api.Models;
+using YngStrs.EmailWorker.Api.Services.Core;
+
+namespace YngStrs.EmailWorker.Api.Services.Business
+{
+    public class SendEmailService : ISendEmailService
+    {
+        private const string GmailHostName = "smtp.gmail.com";
+        private const string GmailUserName = "yngstr1111@gmail.com";
+        private const string GmailPassword = "qa1ws2ed3";
+
+        public async Task SendMailAsync(SendEmailModel model)
+        {
+            using (var mailMessage = new MailMessage(from: GmailUserName, to: model.EmailAddressTo))
+            {
+                mailMessage.Subject = model.Subject;
+                mailMessage.Body = model.Body;
+                mailMessage.IsBodyHtml = false;
+                using (var smtp = new SmtpClient(GmailHostName))
+                {
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = new NetworkCredential(GmailUserName, GmailPassword);
+                    smtp.Port = 587;
+                    await smtp.SendMailAsync(mailMessage);
+                }
+            }
+        }
+    }
+}
