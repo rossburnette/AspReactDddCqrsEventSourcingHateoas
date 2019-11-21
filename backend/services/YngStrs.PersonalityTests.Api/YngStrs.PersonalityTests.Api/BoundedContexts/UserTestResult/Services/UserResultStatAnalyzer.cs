e2c3e265fbe07a39c12c14c8e364a57d50057979
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-21 21:02:57 +0200

    v2.3.4

--- backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/BoundedContexts/UserTestResult/Services/UserResultStatAnalyzer.cs
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/BoundedContexts/UserTestResult/Services/UserResultStatAnalyzer.cs
@@ -18,7 +18,7 @@ namespace YngStrs.PersonalityTests.Api.BoundedContexts.UserTestResult.Services
                 { userResults.Action, nameof(userResults.Action).ToLower() },
                 { userResults.Idea, nameof(userResults.Idea).ToLower() },
                 { userResults.People, nameof(userResults.People).ToLower() },
-                { userResults.Process, nameof(userResults.Process).ToLower() },
+                { userResults.Process, nameof(userResults.Process).ToLower() }
             };
 
             ValuesDictionary = dictionary.ToImmutableSortedDictionary();
@@ -28,5 +28,34 @@ namespace YngStrs.PersonalityTests.Api.BoundedContexts.UserTestResult.Services
         /// Structured user result values.
         /// </summary>
         public ImmutableSortedDictionary<int, string> ValuesDictionary { get; }
+
+        /// <summary>
+        /// Algorithm for extracting the most common result.
+        /// </summary>
+        /// <remarks>
+        /// In this context 'firsSchemeCount', 'secondSchemeCount'
+        /// are the most relevant schemes of the user.
+        /// </remarks>
+        public string[] GetTopResults()
+        {
+            var keyValuePairs = ValuesDictionary.ToList();
+
+            var firsSchemeCount = keyValuePairs[3].Key;
+            var secondSchemeCount = keyValuePairs[2].Key;
+            var thirdSchemeCount = keyValuePairs[1].Key;
+
+            if (firsSchemeCount > secondSchemeCount)
+            {
+                return new[] { keyValuePairs[3].Value };
+            }
+
+            if (firsSchemeCount == secondSchemeCount &&
+                secondSchemeCount == thirdSchemeCount)
+            {
+                return new[] { "complex" };
+            }
+
+            return new[] { keyValuePairs[3].Value, keyValuePairs[2].Value };
+        }
     }
 }
