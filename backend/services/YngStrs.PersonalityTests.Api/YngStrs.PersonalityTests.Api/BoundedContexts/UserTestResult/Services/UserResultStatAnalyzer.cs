36adfbb8fffed49763fee7b6b8cee4fedcd5d04b
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-21 09:15:24 +0200

    v2.3.2

--- backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/BoundedContexts/UserTestResult/Services/UserResultStatAnalyzer.cs
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/BoundedContexts/UserTestResult/Services/UserResultStatAnalyzer.cs
@@ -1,12 +1,28 @@
-﻿using YngStrs.PersonalityTests.Api.Domain.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using YngStrs.PersonalityTests.Api.Domain.Entities;
 
 namespace YngStrs.PersonalityTests.Api.BoundedContexts.UserTestResult.Services
 {
     public class UserResultStatAnalyzer
     {
+        private readonly IOrderedEnumerable<KeyValuePair<string, int>> _valuePairs;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserResultStatAnalyzer"/> class.
+        /// </summary>
+        /// <param name="userResults"></param>
         public UserResultStatAnalyzer(TestResultStatistics userResults)
         {
-            
+            var dictionary = new Dictionary<string, int>
+            {
+                { nameof(userResults.Action).ToLower(), userResults.Action },
+                { nameof(userResults.Idea).ToLower(), userResults.Idea },
+                { nameof(userResults.People).ToLower(), userResults.People },
+                { nameof(userResults.Process).ToLower(), userResults.Process },
+            };
+
+            _valuePairs = dictionary.OrderBy(pair => pair.Value);
         }
     }
 }
