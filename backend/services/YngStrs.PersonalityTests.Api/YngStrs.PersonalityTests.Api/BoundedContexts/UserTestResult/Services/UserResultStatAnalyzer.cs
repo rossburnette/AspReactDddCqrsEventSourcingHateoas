9eaa767784e0229574f4bfa90c1c17b5905a69c0
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-21 08:34:39 +0200

    v2.3.1

---
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api/BoundedContexts/UserTestResult/Services/UserResultStatAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using YngStrs.PersonalityTests.Api.Domain.Entities;
+
+namespace YngStrs.PersonalityTests.Api.BoundedContexts.UserTestResult.Services
+{
+    public class UserResultStatAnalyzer
+    {
+        public UserResultStatAnalyzer(TestResultStatistics userResults)
+        {
+            
+        }
+    }
+}
