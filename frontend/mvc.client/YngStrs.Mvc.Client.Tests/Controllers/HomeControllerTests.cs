0ece06fd47dfd9988ec3471c5f217784b4cbca7d
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-26 22:53:56 +0200

    v2.4.8

---
+++ frontend/mvc.client/YngStrs.Mvc.Client.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using MyTested.AspNetCore.Mvc;
+using Xunit;
+using YngStrs.Mvc.Client.Controllers;
+
+namespace YngStrs.Mvc.Client.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void AboutUs_Should_Return_Default_View()
+            => MyMvc
+                .Controller<HomeController>()
+                .Calling(c => c.AboutUs())
+                .ShouldReturn()
+                .View();
+    }
+}
