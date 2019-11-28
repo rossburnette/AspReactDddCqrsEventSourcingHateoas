26d4abd250523a9e9a57c9cc218257ec13dddbc8
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-28 14:40:49 +0200

    v2.5.8

--- frontend/mvc.client/YngStrs.Mvc.Client.Tests/Controllers/ChatbotControllerTests.cs
+++ frontend/mvc.client/YngStrs.Mvc.Client.Tests/Controllers/ChatbotControllerTests.cs
@@ -1,7 +1,25 @@
-﻿namespace YngStrs.Mvc.Client.Tests.Controllers
+﻿using MyTested.AspNetCore.Mvc;
+using Xunit;
+using YngStrs.Mvc.Client.Controllers;
+
+namespace YngStrs.Mvc.Client.Tests.Controllers
 {
     public class ChatbotControllerTests
     {
-        
+        [Fact]
+        public void Index_Should_Return_Default_View() =>
+            MyMvc
+                .Controller<ChatbotController>()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void Questions_Should_Return_Json() =>
+            MyController<ChatbotController>
+                .Instance()
+                .Calling(controller => controller.Questions())
+                .ShouldReturn()
+                .Json();
     }
 }
--- frontend/mvc.client/YngStrs.Mvc.Client.Tests/Controllers/UsersControllerTests.cs
+++ frontend/mvc.client/YngStrs.Mvc.Client.Tests/Controllers/UsersControllerTests.cs
@@ -1,7 +1,25 @@
-﻿namespace YngStrs.Mvc.Client.Tests.Controllers
+﻿using MyTested.AspNetCore.Mvc;
+using Xunit;
+using YngStrs.Mvc.Client.Controllers;
+
+namespace YngStrs.Mvc.Client.Tests.Controllers
 {
     public class UsersControllerTests
     {
-        
+        [Fact]
+        public void Login_Should_Return_Default_View() =>
+            MyMvc
+                .Controller<UsersController>()
+                .Calling(c => c.Login())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void Register_Should_Return_Default_View() =>
+            MyMvc
+                .Controller<UsersController>()
+                .Calling(c => c.Register())
+                .ShouldReturn()
+                .View();
     }
 }
