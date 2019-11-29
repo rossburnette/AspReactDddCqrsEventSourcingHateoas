70393478312ceb84dc3df3affec5bcd85245f9fa
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-29 15:34:40 +0200

    v2.6.6

---
+++ backend/services/YngStrs.PersonalityTests.Api/YngStrs.PersonalityTests.Api.Tests/Unit/Controllers/PersonalityTestsControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Xunit;
+using YngStrs.Common.Hateoas.Core;
+using YngStrs.PersonalityTests.Api.BoundedContexts.PersonalityTest.Queries;
+using YngStrs.PersonalityTests.Api.Controllers;
+using YngStrs.PersonalityTests.Api.Domain.Views.PersonalityTests;
+using YngStrs.PersonalityTests.Api.Tests.Customizations;
+
+namespace YngStrs.PersonalityTests.Api.Tests.Unit.Controllers
+{
+    public class PersonalityTestsControllerTests
+    {
+        private readonly Mock<IResourceMapper> _resourceMapperMock;
+        private readonly Mock<IMediator> _mediatorMock;
+
+        private readonly PersonalityTestsController _controller;
+
+        public PersonalityTestsControllerTests()
+        {
+            _resourceMapperMock = new Mock<IResourceMapper>();
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new PersonalityTestsController(_resourceMapperMock.Object, _mediatorMock.Object);
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task GetStructuredPersonalityTest_Returns_200_Ok(RootTestView testData)
+        {
+            // Arrange
+            _mediatorMock
+                .Setup(mediator => mediator.Send(new GetStructuredTest(), CancellationToken.None))
+                .ReturnsAsync(() => testData);
+
+            // Act
+            var result = await _controller.GetStructuredPersonalityTest();
+
+            // Assert 
+            result
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
