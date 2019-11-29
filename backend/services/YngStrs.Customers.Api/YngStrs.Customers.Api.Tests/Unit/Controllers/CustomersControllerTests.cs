6a7f9688cede04a5991e63f570e03fa60a16f013
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-29 16:14:02 +0200

    v2.6.7

---
+++ backend/services/YngStrs.Customers.Api/YngStrs.Customers.Api.Tests/Unit/Controllers/CustomersControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+using YngStrs.Customers.Api.Controllers;
+
+namespace YngStrs.Customers.Api.Tests.Unit.Controllers
+{
+    public class CustomersControllerTests
+    {
+        private readonly CustomerProvider _provider;
+
+        private readonly CustomersController _controller;
+
+        public CustomersControllerTests()
+        {
+            _provider = new CustomerProvider();
+            _controller = new CustomersController(_provider);
+        }
+
+        [Fact]
+        public void Get_Returns_200_Ok()
+        {
+            // Act 
+            var result = _controller.Get();
+
+            // Assert
+            result
+                .Value
+                .Should()
+                .NotBeNullOrEmpty();
+        }
+    }
+}
