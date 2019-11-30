5cb8f1b08ace9cb5243114ac044957543777f0c8
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-30 19:41:45 +0200

    v2.6.9

---
+++ backend/services/YngStrs.Common.Contents.Models/YngStrs.Common.Contents.Models/Home/HomeIntroChatBotSection.cs
@@ -0,0 +1,15 @@
+﻿namespace YngStrs.Common.Contents.Models.Home
+{
+    public class HomeIntroChatBotSection
+    {
+        public string SpanWriteMe { get; set; }
+
+        public string WrapTextTitleBigBlack { get; set; }
+
+        public string WrapTextTitleBigRed { get; set; }
+
+        public string WrapTextPsmall { get; set; }
+
+        public string BtnBeginTheInterview { get; set; }
+    }
+}
