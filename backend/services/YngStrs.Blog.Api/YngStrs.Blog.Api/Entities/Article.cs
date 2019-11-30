0964d300a1da0f5d1435fffb409afd0064703a2b
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-30 20:36:45 +0200

    v2.7.2

---
+++ backend/services/YngStrs.Blog.Api/YngStrs.Blog.Api/Entities/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using YngStrs.Common.Api;
+
+namespace YngStrs.Blog.Api.Entities
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public ProjectLanguages Language { get; set; }
+    }
+}
---
+++ backend/services/YngStrs.Blog.Api/YngStrs.Blog.Api/Persistence/BlogDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YngStrs.Blog.Api.Entities;
+
+namespace YngStrs.Blog.Api.Persistence
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Article>()
+                .Property(a => a.Id)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder
+                .Entity<Article>()
+                .ToTable("articles");
+
+            modelBuilder
+                .Entity<Article>()
+                .Property(a => a.Id)
+                .HasColumnName("id");
+
+            modelBuilder
+               .Entity<Article>()
+               .Property(a => a.Language)
+               .HasColumnName("language");
+
+            modelBuilder
+               .Entity<Article>()
+               .Property(a => a.Title)
+               .HasColumnName("title");
+
+            modelBuilder
+               .Entity<Article>()
+               .Property(a => a.Content)
+               .HasColumnName("content");
+        }
+    }
+}
