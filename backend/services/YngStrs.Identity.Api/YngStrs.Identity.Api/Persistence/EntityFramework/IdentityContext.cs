2b5657589173d71a8a2af34af124d3f1d889fd22
Jordan Jordanov <d_yugioh@abv.bg>
2019-12-02 11:25:38 +0200

    v2.8.0

--- backend/services/YngStrs.Identity.Api/YngStrs.Identity.Api/Persistence/EntityFramework/IdentityContext.cs
+++ backend/services/YngStrs.Identity.Api/YngStrs.Identity.Api/Persistence/EntityFramework/IdentityContext.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using YngStrs.Identity.Api.Domain.Entities;
 
 namespace YngStrs.Identity.Api.Persistence.EntityFramework
 {
     public class IdentityContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
-
+        /// <summary>
+        /// Initializes an instance of the <see cref="IdentityContext"/> class.
+        /// </summary>
+        /// <param name="options">The database context options.</param>
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
     }
 }
