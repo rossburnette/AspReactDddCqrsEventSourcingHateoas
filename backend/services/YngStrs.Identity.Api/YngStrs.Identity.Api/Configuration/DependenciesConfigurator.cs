85d4f7b255c7f8e3e52d17681d71f97fc9b060bd
Jordan Jordanov <d_yugioh@abv.bg>
2019-12-03 08:55:37 +0200

    v2.8.5

--- backend/services/YngStrs.Identity.Api/YngStrs.Identity.Api/Configuration/DependenciesConfigurator.cs
+++ backend/services/YngStrs.Identity.Api/YngStrs.Identity.Api/Configuration/DependenciesConfigurator.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Marten;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RiskFirst.Hateoas;
 using YngStrs.Common.Api.DatabaseConnectors;
 using YngStrs.Common.Cqrs.Business;
 using YngStrs.Common.Cqrs.Core;
+using YngStrs.Common.EventSourcing.Business;
+using YngStrs.Common.EventSourcing.Core;
+using YngStrs.Common.Hateoas.Business;
+using YngStrs.Common.Hateoas.Core;
+using YngStrs.Identity.Api.Domain.Repositories;
+using YngStrs.Identity.Api.Persistence.Repositories;
 using YngStrs.Identity.Api.Settings;
 
 namespace YngStrs.Identity.Api.Configuration
@@ -41,6 +52,98 @@ namespace YngStrs.Identity.Api.Configuration
         internal static IServiceCollection AddDbConnectors(this IServiceCollection serviceCollection)
         {
             return serviceCollection.AddScoped<IQueryDbConnector, QueryDbConnector>();
+        }
+
+        internal static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            var repositoryTypes = Assembly
+                .GetAssembly(typeof(IUserRepository))
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("Repository") &&
+                            t.IsInterface)
+                .ToArray();
+
+            var repositoryImplementationTypes = Assembly
+                .GetAssembly(typeof(UserRepository))
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("Repository") &&
+                            t.IsClass)
+                .ToDictionary(t => t.Name, t => t);
+
+            foreach (var repositoryType in repositoryTypes)
+            {
+                var expectedImplementationName = repositoryType
+                    .Name
+                    .Substring(1);
+
+                if (!repositoryImplementationTypes.ContainsKey(expectedImplementationName))
+                {
+                    throw new InvalidOperationException($"Could not find implementation for {repositoryType.FullName}.");
+                }
+
+                var implementation = repositoryImplementationTypes[expectedImplementationName];
+
+                if (!repositoryType.IsAssignableFrom(implementation))
+                {
+                    throw new InvalidOperationException($"For repository {repositoryType.Name} found matching type {implementation.Name}, but it does not implement it.");
+                }
+
+                services.AddScoped(repositoryType, implementation);
+            }
+
+            return services;
+        }
+
+        internal static IServiceCollection AddEventSourcing(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var documentStore = DocumentStore.For(options =>
+            {
+                var config = configuration.GetSection("EventStore");
+                var connectionString = config.GetValue<string>("ConnectionString");
+                var schemaName = config.GetValue<string>("Schema");
+
+                options.Connection(connectionString);
+                options.AutoCreateSchemaObjects = AutoCreate.All;
+                options.Events.DatabaseSchemaName = schemaName;
+                options.DatabaseSchemaName = schemaName;
+            });
+
+            services.AddSingleton<IDocumentStore>(documentStore);
+
+            services.AddScoped(sp => sp.GetService<IDocumentStore>().OpenSession());
+
+            services.AddScoped<IEventBus, EventBus>();
+
+            return services;
+        }
+
+        internal static IServiceCollection AddHateoas(this IServiceCollection services)
+        {
+            services.AddTransient<IResourceMapper, ResourceMapper>();
+
+            services.AddLinks(config =>
+            {
+                var policies = Assembly
+                    .GetAssembly(typeof(DependenciesConfigurator))
+                    .GetTypes()
+                    .Where(t => !t.IsInterface &&
+                                !t.IsAbstract &&
+                                t.GetInterfaces().Any(i => i.Name == typeof(IPolicy<>).Name))
+                    .Select(Activator.CreateInstance)
+                    .Cast<dynamic>()
+                    .ToArray();
+
+                foreach (var policy in policies)
+                {
+                    config.AddPolicy(policy.PolicyConfiguration);
+                }
+            });
+
+            return services;
+
+            return services;
         }
     }
 }
