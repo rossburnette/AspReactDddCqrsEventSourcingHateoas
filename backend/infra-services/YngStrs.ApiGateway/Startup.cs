1f7f405a3f419cb592d8b85bcb366897694409f6
Jordan Jordanov <d_yugioh@abv.bg>
2020-01-10 11:21:40 +0200

    v3.1.4

--- backend/infra-services/YngStrs.ApiGateway/Startup.cs
+++ backend/infra-services/YngStrs.ApiGateway/Startup.cs
@@ -1,40 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace YngStrs.ApiGateway
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private const string CorsPolicyName = "CorsPolicy";
+
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(env.ContentRootPath)
+                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicyName, builder =>
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddOcelot(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(CorsPolicyName);
 
             app.UseHttpsRedirection();
 
@@ -46,6 +55,11 @@ namespace YngStrs.ApiGateway
             {
                 endpoints.MapControllers();
             });
+
+            app
+                .UseOcelot()
+                .GetAwaiter()
+                .GetResult();
         }
     }
 }
